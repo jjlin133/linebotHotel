71f74d22f3fac43d5b9738728aee302dbbdfc875
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2020-05-14 23:39:00 +0800

    Update admin.py

--- hotelapi/admin.py
+++ hotelapi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from hotelapi.models import users,booking
+
 
 # Register your models here.
+# 第一種方式，未加入 ModelAdmin 類別 
+admin.site.register(users)
+admin.site.register(booking)
